aad0e1209f19408f07195e271bfaea04588b1180
gustjd887 <gustjd887@gmail.com>
2023-04-30 12:23:19 +0900

    crud

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -49,7 +49,19 @@ public class BoardController {
     }
 
     @GetMapping("/board/modify/{id}")
-    public String boardModify(@PathVariable("id") Integer id) {
+    public String boardModify(Model model, @PathVariable("id") Integer id) {
+        model.addAttribute("board", boardService.boardView(id));
         return "boardmodify";
+    }
+
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, Board board) {
+        Board boardTemp = boardService.boardView(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+
+        boardService.write(boardTemp);
+
+        return "redirect:/board/list";
     }
 }
